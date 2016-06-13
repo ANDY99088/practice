e10ec9b8f62eb7f053e8634f99ed5a27895a3420
ANDY99088 <zhaoce@Mario.local>
2016-06-13 10:12:50 +0800

    改了你的XXXOOO

---
+++ X_jihua-master/X_jihua1/X_jihua1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  X_jihua1
+//
+//  Created by Saina on 16/6/13.
+//  Copyright © 2016年 Saina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ X_jihua-master/X_jihua1/X_jihua1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  X_jihua1
+//
+//  Created by Saina on 16/6/13.
+//  Copyright © 2016年 Saina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
