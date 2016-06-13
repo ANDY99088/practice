af6d8de72d525c519369f8faaecbcb53c82565c1
ANDY99088 <zhaoce@Mario.local>
2016-06-13 09:45:18 +0800

    我改了

---
+++ practice2/practice2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practice2
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practice2/practice2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice2
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
